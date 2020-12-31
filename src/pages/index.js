16f26d2300e24f8af373a351482329fcd0d478f8
Troy Porter <porter@fb.com>
2020-12-30 22:44:50 -0500

    initial commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
 
-export default function Home() {
-  return <div>Hello world!</div>
+const Home = () => {
+  // Client-side Runtime Data Fetching
+  // const [starsCount, setStarsCount] = useState(0)
+  useEffect(() => {
+    // get data from GitHub api
+    fetch(`https://reddit.com/r/spacs/hot.json`, { mode: "cors" }).then(
+      response => {
+        console.log(response)
+      }
+    )
+    // .then(resultData => {
+    //   console.log({ resultData })
+    // })
+  }, [])
+  return (
+    <section>
+      <p>Runtime Data: Star count for the Gatsby repo</p>
+    </section>
+  )
 }
+export default Home
