3e4a6a5d87a77fb36d4fe7d66101005691b72bc0
Troy Porter <porter@fb.com>
2020-12-30 23:33:30 -0500

    reddit scraper basic example

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,22 +1,31 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 
 const Home = () => {
   // Client-side Runtime Data Fetching
-  // const [starsCount, setStarsCount] = useState(0)
+  const [spacPosts, setSpacPosts] = useState(null)
   useEffect(() => {
     // get data from GitHub api
-    fetch(`https://reddit.com/r/spacs/hot.json`, { mode: "cors" }).then(
-      response => {
-        console.log(response)
-      }
-    )
-    // .then(resultData => {
-    //   console.log({ resultData })
-    // })
+    fetch(`https://www.reddit.com/r/spacs/hot.json`)
+      .then(response => response.json()) // parse JSON from request
+      .then(resultData => {
+        console.log(resultData)
+        setSpacPosts(resultData.data.children)
+      })
   }, [])
+  console.log(spacPosts)
   return (
     <section>
-      <p>Runtime Data: Star count for the Gatsby repo</p>
+      <h1>Reddit posts from SPAC</h1>
+      <ul>
+        {spacPosts &&
+          spacPosts.map(p => (
+            <li key={p?.data?.id}>
+              <a href={`https://www.reddit/com${p?.data?.permalink}`}>
+                {p?.data?.title}
+              </a>
+            </li>
+          ))}
+      </ul>
     </section>
   )
 }
